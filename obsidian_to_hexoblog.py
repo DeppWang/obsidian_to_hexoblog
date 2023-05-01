6a68c62f50ae160a45beda82baaa90b46a97a88f
deppwang <deppwxq@gmail.com>
2023-05-01 12:55:52 +0800

    Updated: 2023-05-01 12:55:52

--- obsidian_to_hexoblog.py
+++ obsidian_to_hexoblog.py
@@ -72,7 +72,7 @@ def is_need_post_hexo(post_article_path, english_title, file_path):
         # Decode the output and extract the date and time string
         git_last_commit_date_str = git_log_result.stdout.decode().strip()
         print(git_last_commit_date_str)
-        dt = datetime.strptime(git_last_commit_date_str, STR_FORMAT_DATETIME2)
+        dt = datetime.datetime.strptime(git_last_commit_date_str, STR_FORMAT_DATETIME2)
         file_git_last_commit_date = dt.strptime(git_last_commit_date_str, STR_FORMAT_DATETIME)
         return create_time, file_git_last_commit_date, file_git_last_commit_date != hexo_update_time
     return create_time, '', False
