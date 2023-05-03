aad30f4b9e8d394710da2d57505946d0ece706f1
Depp Wang <deppwxq@gmail.com>
2023-05-03 22:58:02 +0800

    Update obsidian_to_hexoblog.py

--- obsidian_to_hexoblog.py
+++ obsidian_to_hexoblog.py
@@ -62,8 +62,8 @@ def is_need_post_hexo(post_article_path, english_title, file_path):
         date_result = DATE_REGEX.findall(content)
         create_time_str = date_result[0]
         # HexoBlog 最后更新时间
-        update_time_str = date_result[1] if len(date_result) == 2 else date_result[0]
-        print(date_result, create_time_str, update_time_str)
+        update_time_str = date_result[1] if len(date_result) >= 2 else date_result[0]
+        update_time_str = update_time_str if update_time_str else create_time_str
         hexo_update_time = datetime.datetime.strptime(update_time_str, STR_FORMAT_DATETIME)
         # Run the Git command to get the last commit date for the file
         git_log_result = subprocess.run(["git", "log", "-1", "--format=%cd", "--",
@@ -73,7 +73,7 @@ def is_need_post_hexo(post_article_path, english_title, file_path):
         dt = datetime.datetime.strptime(git_last_commit_date_str, STR_FORMAT_DATETIME2)
         file_git_last_commit_date = dt.strftime(STR_FORMAT_DATETIME)
         # 如果 hexoblog 中的 update_date 和文件最后更新时间不相同，则需要更新
-        print('file_git_last_commit_date: %s, hexo_update_time: %s', file_git_last_commit_date, hexo_update_time)
+        print('file_git_last_commit_date: %s, hexo_update_time: %s' % (file_git_last_commit_date, hexo_update_time))
         return create_time_str, file_git_last_commit_date, file_git_last_commit_date != hexo_update_time
     # 如果不存在，则需求发布到 HexoBlog
     return create_time_str, create_time_str, True
